42d7341da570b2f95f5921dc3f7b5696b2f54094
Aaron Schendel <aaron.schendel@gmail.com>
2021-03-25 22:58:17 -0500

    Async working

--- hiveosApi.js
+++ hiveosApi.js
@@ -5,26 +5,39 @@ class HiveosApi {
 
     constructor(bearerToken) {
         this.bearerToken = bearerToken;
-        this.farmId = this.getFarmId();
     }
 
-    getFarmId(){
-        const options = {
-            url: 'https://api2.hiveos.farm/api/v2/farms',
-            method: 'GET',
-            headers: { 'Authorization': `Bearer ${this.bearerToken}` }
-        };
-        
-        req(options, function(err, res, body) {
+    async getFarmId(){
+        try {
+            let myVar = '';
+            const options = {
+                url: 'https://api2.hiveos.farm/api/v2/farms',
+                method: 'GET',
+                headers: { 'Authorization': `Bearer ${this.bearerToken}` }
+            };
+            
+            let body = await this.doRequest(options);
             let json = JSON.parse(body);
-            const myVar = json.data[0].id;
+            myVar = json.data[0].id;
             console.log(myVar);
-        });
+            // req(options, function (err, res, body) {
+            //     let json = JSON.parse(body);
+            //     myVar = json.data[0].id;
+            //     console.log(myVar);
+            // });
+
+            return myVar;
+        }
+        catch (e) {
+            console.log('Catch an error: ', e)
+            return '1111111';
+        }
     }
 
-    isOnline() {
+    async isOnline() {
+        let farmId = await this.getFarmId().then(console.log('Got a result from getFarmId'));
         const options = {
-            url: `https://api2.hiveos.farm/api/v2/farms/481439/workers`,
+            url: `https://api2.hiveos.farm/api/v2/farms/${farmId}/workers`,
             method: 'GET',
             headers: { 'Authorization': `Bearer ${this.bearerToken}` }
         };
@@ -35,6 +48,19 @@ class HiveosApi {
             return myVar;
         });
     }
+
+
+    async doRequest(options) {
+        return new Promise(function (resolve, reject) {
+          req(options, function (error, res, body) {
+            if (!error && res.statusCode == 200) {
+              resolve(body);
+            } else {
+              reject(error);
+            }
+          });
+        });
+      }
 
 }
 
--- statusmonitor.js
+++ statusmonitor.js
@@ -8,21 +8,27 @@ const bearerToken = authorize();
 const hiveOsApi = new hapi.HiveosApi(bearerToken);
 
 
-//beginPolling(bearerToken);
+beginPolling(bearerToken);
 
 
-hiveOsApi.isOnline();
+
 
 function authorize() {
     var configJson = JSON.parse(fs.readFileSync('hiveos_config.json', 'utf8'));
     const bearerToken = configJson.credentials.bearer_token;
 
-    return bearerToken
+    return bearerToken;
 }
 
 
-function beginPolling(bearerToken) {
-    AsyncPolling(function(end) {
+async function beginPolling(bearerToken) {
+    try {
+        await hiveOsApi.isOnline();
+    }
+    catch(e) {
+        console.log(e);
+    }
+    // AsyncPolling(function(end) {
         
-    })
+    // });
 }
