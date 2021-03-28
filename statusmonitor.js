41af0475fc70ffae9483c5fe90a0e2d30df8ad9c
Aaron Schendel <aaron.schendel@gmail.com>
2021-03-27 20:24:34 -0500

    Fully functional

--- statusmonitor.js
+++ statusmonitor.js
@@ -1,42 +1,47 @@
+//============ CONFIG/ENV VARS ==========
 const fs = require('fs');
-const AsyncPolling = require('async-polling');
+const twilio = require('twilio');
 const hapi = require('./hiveosApi');
 
-
-const bearerToken = authorize();
+var configJson = JSON.parse(fs.readFileSync('oshamonitor_config.json', 'utf8'));
+const bearerToken = configJson.credentials.bearer_token;
+const alertRecipient = configJson.twilio.alert_recipient;
+const twilioNumber = configJson.twilio.twilio_number;
+const twilioAccountSid = configJson.twilio.account_sid; 
+const twilioAuthToken = configJson.twilio.auth_token;
 
 const hiveOsApi = new hapi.HiveosApi(bearerToken);
+const twilioClient = new twilio(twilioAccountSid, twilioAuthToken);
+//========================================
 
 
-beginPolling(bearerToken);
+beginPolling();
 
-
-
-
-function authorize() {
-    var configJson = JSON.parse(fs.readFileSync('hiveos_config.json', 'utf8'));
-    const bearerToken = configJson.credentials.bearer_token;
-
-    return bearerToken;
-}
-
-
-async function beginPolling(bearerToken) {
+async function beginPolling() {
     try {
-        setInterval(async function() {
+        var myInterval = setInterval(async function() {
             let online = await hiveOsApi.isOnline();
 
-            if (online === true) {
-                console.log('Nice!');
+            if (online === false) {
+                console.log('Your worker is online and doing their job!');
             }
             else {
                 console.log(online);
-                clearInterval();
+                sendText('Mining rig is offline! Fix this!', alertRecipient, twilioNumber)
+                
+                clearInterval(myInterval); // End polling if offline to avoid sending many texts
             }
         }, 5000);
     }
     catch(e) {
         console.log(e);
     }
-    
+}
+
+function sendText(message, to, from) {
+    twilioClient.messages.create({
+        body: message,
+        to: to, 
+        from: from
+    }).then((message) => console.log(message.sid));
 }
